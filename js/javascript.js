da87bb016a8f241a95ee70e790e5ba8a2382438b
Lucas Barros <lucasg.barros.94@gmail.com>
2022-07-28 19:37:17 -0300

    Add new space count and adjustement in mobiles devices

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,12 @@
 $('textarea').on('keyup paste', function(){
 	setTimeout(function() {
 		var valorTextarea = $('textarea').val().length,
-				palavras = $('textarea').val().split(' ').length;
+				palavras = $('textarea').val().split(' '&&'\n').length;
                 linhas = $('textarea').val().split('\n').length;
+				espacos = $('textarea').val().split(' ').length - 1;
 		$('.caracteres').text(valorTextarea);
 		$('.palavras').text(palavras);
         $('.linhas').text(linhas);
+		$('.espacos').text(espacos);
 	}, 200);
 });
