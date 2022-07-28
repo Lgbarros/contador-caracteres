d1c25220f4f585343bd8883e612a59d3de2a5a75
Lucas Barros <lucasg.barros.94@gmail.com>
2022-07-27 21:56:48 -0300

    Add line count

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,9 @@ $('textarea').on('keyup paste', function(){
 	setTimeout(function() {
 		var valorTextarea = $('textarea').val().length,
 				palavras = $('textarea').val().split(' ').length;
+                linhas = $('textarea').val().split('\n').length;
 		$('.caracteres').text(valorTextarea);
 		$('.palavras').text(palavras);
+        $('.linhas').text(linhas);
 	}, 200);
 });
